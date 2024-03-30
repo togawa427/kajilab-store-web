aee4acb0730c7fc79568617f85d737f59d43a1dd
togawa427 <95838999+togawa427@users.noreply.github.com>
2024-03-31 00:37:56 +0900

    chore: css関連の設定ファイルの設定

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,40 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
+
+const customColors = {
+  forest: {
+    50: '#0A5137',
+    100: '#0E6445',
+    200: '#137752',
+    300: '#18895F',
+    400: '#1D9A6C',
+    500: '#3FAC84',
+    600: '#62BE9C',
+    700: '#86CEB4',
+    800: '#AADECB',
+    900: '#CFEDE2',
+    DEFAULT: '#3FAC84',
+  },
+}
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      colors: {
+        forest: customColors.forest,
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+
+export default config
