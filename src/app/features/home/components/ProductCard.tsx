79fbeb60203cc92e26d5f33d61b3c77c73dae008
togawa427 <95838999+togawa427@users.noreply.github.com>
2024-04-08 11:36:09 +0900

    feat: 管理者以外のページでも商品画像をCloudflareStorageからもってくるようにした

--- src/app/features/home/components/ProductCard.tsx
+++ src/app/features/home/components/ProductCard.tsx
@@ -7,7 +7,7 @@ type ProductCardListProps = {
 }
 
 const ProductCard = ({product}: ProductCardListProps) => {
-  const imgPath = `products/${product.barcode}.jpg`
+  const imgPath = `https://firebasestorage.googleapis.com/v0/b/kajilab-store.appspot.com/o/images%2F${product.barcode}.jpg?alt=media&token=c46357bc-f29c-4d5f-8048-33c1d4a65083`
   return (
     <Card 
       shadow="sm"
