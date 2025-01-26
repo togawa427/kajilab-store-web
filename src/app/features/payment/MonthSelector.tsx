9562dc463917353f626846c3e398f2088fab8eb5
togawa427 <95838999+togawa427@users.noreply.github.com>
2025-01-26 23:03:30 +0900

    feat: 購入ログ画面の次の月へと前の月へのページング機能実装

--- src/app/features/payment/MonthSelector.tsx
+++ src/app/features/payment/MonthSelector.tsx
@@ -1,5 +1,7 @@
+import { Button } from '@mantine/core';
 import { IconChevronLeft, IconChevronRight } from '@tabler/icons-react'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 type MonthSelectorPropsType = {
@@ -9,14 +11,35 @@ type MonthSelectorPropsType = {
 }
 
 function MonthSelector({year, month, salesMonth}: MonthSelectorPropsType) {
+  const router = useRouter();
+
+  const prevMonth = () => {
+    const currentDate = new Date(year, month-1);
+    const prevMonthDate = new Date(currentDate);
+    prevMonthDate.setMonth(currentDate.getMonth() - 1);
+    router.push(`/log/payment/${prevMonthDate.getFullYear()}/${prevMonthDate.getMonth()+1}`)
+    router.refresh()
+  }
+
+  const nextMonth = () => {
+    const currentDate = new Date(year, month-1);
+    console.log(currentDate.getMonth())
+    const prevMonthDate = new Date(currentDate);
+    prevMonthDate.setMonth(currentDate.getMonth()+1);
+    console.log(prevMonthDate.getMonth())
+    console.log(prevMonthDate)
+    router.push(`/log/payment/${prevMonthDate.getFullYear()}/${prevMonthDate.getMonth()+1}`)
+    router.refresh()
+  }
+
   return (
     <div className="text-2xl flex justify-center">
-      <Link href={`/log/payment/2024/12`}><IconChevronLeft size={60} color='blue'/></Link>
+      <Button variant="transparent" onClick={prevMonth}><IconChevronLeft size={60} color='blue'/></Button>
       <div className='w-1/3 text-center'>
         <div>{year}年{month}月</div>
         <div className="text-blue-800 font-bold">収入：¥ {salesMonth}</div>
       </div>
-      <Link href={`/log/payment/2025/1`}><IconChevronRight size={60} color='blue'/></Link>
+      <Button variant="transparent" onClick={nextMonth}><IconChevronRight size={60} color='blue'/></Button>
     </div>
   )
 }
