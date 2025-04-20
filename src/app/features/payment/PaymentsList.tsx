1a82791b21a3f87f629ead24bbd50634bffa1414
togawa427 <95838999+togawa427@users.noreply.github.com>
2025-04-21 00:42:35 +0900

    fix: 月間売り上げの更新日の月が+1していなかったのを修正

--- src/app/features/payment/PaymentsList.tsx
+++ src/app/features/payment/PaymentsList.tsx
@@ -12,7 +12,7 @@ type PaymentListsPropsType = {
 function PaymentsList({salesDays, month, year, responseDate}: PaymentListsPropsType) {
   return (
     <div className="mt-5 mb-20">
-      <div className='text-end md:text-base text-sm'>更新日時：{responseDate.getFullYear()}年{responseDate.getMonth()}月{String(responseDate.getDate()).padStart(2, '0')}日 {String(responseDate.getHours()).padStart(2, '0')}:{String(responseDate.getMinutes()).padStart(2, '0')}:{String(responseDate.getSeconds()).padStart(2, '0')}</div>
+      <div className='text-end md:text-base text-sm'>更新日時：{responseDate.getFullYear()}年{responseDate.getMonth()+1}月{String(responseDate.getDate()).padStart(2, '0')}日 {String(responseDate.getHours()).padStart(2, '0')}:{String(responseDate.getMinutes()).padStart(2, '0')}:{String(responseDate.getSeconds()).padStart(2, '0')}</div>
       <div className='text-end md:text-base text-sm'>（更新頻度：約1時間）</div>
       {salesDays.map((salesDay) => (
         <PaymentComponent.PaymentByDay
