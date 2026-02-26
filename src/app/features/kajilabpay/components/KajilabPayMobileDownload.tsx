c08ef8e061b2d099064c0f9b6b0ec017e27086ed
togawa427 <95838999+togawa427@users.noreply.github.com>
2026-02-26 11:41:49 +0900

    feat: 梶研Payモバイルの再生成を気軽にされないように作成し直すボタンを削除

--- src/app/features/kajilabpay/components/KajilabPayMobileDownload.tsx
+++ src/app/features/kajilabpay/components/KajilabPayMobileDownload.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@mantine/core'
+import { Button, Space } from '@mantine/core'
 import React from 'react'
 import * as KajilabPayComponent from "@/app/features/kajilabpay/components/Index"
 
@@ -10,23 +10,24 @@ type KajilabPayMobileDownloadProps = {
 
 export default function KajilabPayMobileDownload({userBarcode, name, qrcodeUrl}: KajilabPayMobileDownloadProps) {
   // LocalStorageの削除
-  const removeLocalStorage = () => {
-    localStorage.removeItem("kajilabpaymobile")
-    window.location.reload()
-  }
+  // const removeLocalStorage = () => {
+  //   localStorage.removeItem("kajilabpaymobile")
+  //   window.location.reload()
+  // }
   return (
     <div>
       <p>下の画像をダウンロードして完了です</p>
-      <p className='mb-2'>引き続き梶研Payカードは利用可能です</p>
+      <p className='mb-2'>商店レジで残高チャージしてからお使いください</p>
       <KajilabPayComponent.KajilabPayMobileCanvas
         userBarcode={userBarcode}
         name={name}
         qrcodeUrl={qrcodeUrl}
       />
+      <Space h='10px'/>
 
-      <Button color="#FADA0A" className='mt-10 text-gray-900' onClick={removeLocalStorage}>
+      {/* <Button color="#FADA0A" className='mt-10 text-gray-900' onClick={removeLocalStorage}>
         作成しなおす
-      </Button>
+      </Button> */}
 
     </div>
   )
